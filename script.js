4326557fa1057697b7b1daec39a0c8e3e2ea8805
Elliot <elliot.kodehode@gmail.com>
2024-02-22 15:15:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/** @format */
+
+const gradients = document.getElementsByClassName("gradient");
+const linkedIn = document.getElementById("linked-in");
+
+for (let i = 0; i < gradients.length; i++) {
+  gradients[i].style.fill = "#fff0ff";
+}
