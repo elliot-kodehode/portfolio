b20bc847b334a56b9193f146e09b0a2e39c9d775
Elliot <elliot.kodehode@gmail.com>
2024-02-25 02:21:33 +0100

    working on navbar

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ const gradients = document.getElementsByClassName("gradient");
 const linkedIn = document.getElementById("linked-in");
 
 for (let i = 0; i < gradients.length; i++) {
-  gradients[i].style.fill = "#fff0ff";
+  gradients[i].style.fill = "#22FFE4";
+  gradients[i].style.color = "#22FFE4";
 }
