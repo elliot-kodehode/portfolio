90d011252871245c9e9c6fcd308186d9d0599943
Elliot <elliot.kodehode@gmail.com>
2024-02-27 23:14:36 +0100

    started on a function :D

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 const gradients = document.getElementsByClassName('gradient2')
 const linkedIn = document.getElementById('linked-in')
+const languageSwitch = document.querySelector(".toggle-mode")
+const modeIcon = document.getElementById("mode-icon")
+const iconPath = document.getElementById("icon-path")
 
 function colorPicker() {
 	// const gradientColours = ['#9747FF', '#2DFF81', '#FF9A51']
@@ -10,11 +13,20 @@ function colorPicker() {
 	// console.log(chosenColor)
 
 	// setting text-color gradient
-	element.style.backgroundImage = 'linear-gradient(to right, red, blue)'
-	element.style.WebkitBackgroundClip = 'text'
-	element.style.color = 'transparent'
+	for (let i = 0; i < gradients.length; i++) {
+		// setting text-color gradient for each element
+		gradients[i].style.backgroundImage = 'linear-gradient(to right, magenta, orange )';
+		gradients[i].style.WebkitBackgroundClip = 'text';
+		gradients[i].style.color = 'transparent';
 }
-
+}
 // linear-gradient(#2DFF81, #9747FF)
 
 colorPicker()
+
+const changeMode = () => {
+}
+;
+
+
+languageSwitch.addEventListener("click", changeMode)
