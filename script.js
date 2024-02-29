05687cbdd41fc95f1b0e231d8b6e59c28fe6d79a
Elliot <elliot.kodehode@gmail.com>
2024-02-29 11:36:33 +0100

    svg IS the goat

--- script.js
+++ script.js
@@ -1,30 +1,67 @@
 /** @format */
+// import githubLogo from 'icons/github.svg'
 
-const gradients = document.getElementsByClassName('gradient2')
+const gradientText = document.getElementsByClassName('gradient-text')
+const gradientIcon = document.getElementsByClassName('gradient-icon')
 const linkedIn = document.getElementById('linked-in')
 const languageSwitch = document.querySelector(".toggle-mode")
 const modeIcon = document.getElementById("mode-icon")
-const iconPath = document.getElementById("icon-path")
+// const iconPath = document.querySelector(".icon-path")
 
 function colorPicker() {
-	// const gradientColours = ['#9747FF', '#2DFF81', '#FF9A51']
+	const gradientColours = [
+		{ start: "#c516cd", end: "#00d4ff" },
+		{ start: "#731ee9", end: "#24e324" },
+		{ start: "#ffee28", end: "#ff361c" },
+		{ start: "#255ddb", end: "#06ff65" }
+	]
 
-	// let chosenColor = gradientColours[Math.floor(Math.random() * gradientColours.length)]
-	// console.log(chosenColor)
+	const chosenColors = gradientColours[Math.floor(Math.random() * gradientColours.length)]
+	console.log(chosenColors)
 
 	// setting text-color gradient
-	for (let i = 0; i < gradients.length; i++) {
+	for (let i = 0; i < gradientText.length; i++) {
 		// setting text-color gradient for each element
-		gradients[i].style.backgroundImage = 'linear-gradient(to right, magenta, orange )';
-		gradients[i].style.WebkitBackgroundClip = 'text';
-		gradients[i].style.color = 'transparent';
-}
+		gradientText[i].style.backgroundImage = `linear-gradient(60deg, ${chosenColors.start}, ${chosenColors.end})`;
+		gradientText[i].style.WebkitBackgroundClip = 'text';
+		gradientText[i].style.color = 'transparent';
+	}
+
+	// setting svg color
+	for (let i = 0; i < gradientIcon.length; i++) {
+		const gradient = document.createElementNS("http://www.w3.org/2000/svg", "linearGradient");
+		gradient.setAttribute("id", `icon-gradient-${i}`);
+		gradient.setAttribute("x1", "0%");
+		gradient.setAttribute("y1", "0%");
+		gradient.setAttribute("x2", "100%");
+		gradient.setAttribute("y2", "100%");
+
+		const startStop1 = document.createElementNS("http://www.w3.org/2000/svg", "stop");
+		startStop1.setAttribute("offset", "0%");
+		startStop1.setAttribute("style", `stop-color: ${chosenColors.start}`);
+
+		const startStop2 = document.createElementNS("http://www.w3.org/2000/svg", "stop");
+		startStop2.setAttribute("offset", "100%");
+		startStop2.setAttribute("style", `stop-color: ${chosenColors.end}`);
+
+		gradient.appendChild(startStop1);
+		gradient.appendChild(startStop2);
+
+		gradientIcon[i].appendChild(gradient)
+
+		const iconPaths = gradientIcon[i].getElementsByTagName("path");
+		for (let j = 0; j < iconPaths.length; j++) {
+			iconPaths[j].setAttribute("fill", `url(#icon-gradient-${i})`);
+		}
+
+	}
 }
 // linear-gradient(#2DFF81, #9747FF)
 
 colorPicker()
 
 const changeMode = () => {
+
 }
 ;
 
